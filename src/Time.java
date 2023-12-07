5e9d7401a5434250d1894ff612d7accdd2fd31eb
BT_2N2_02 <sample@bths.edu>
2023-12-07 13:58:29 -0500

    initial commit

---
+++ src/Time.java
@@ -0,0 +1,23 @@
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public void tick() {
+
+    }
+
+    public void add(Time time2) {
+
+    }
+
+    public String info() {
+        return "";
+    }
+}
