c24e745ac5c6fab782c15b4094491422c5051d89
BT_2N2_02 <sample@bths.edu>
2023-12-12 13:53:03 -0500

    finished methods

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Time time4 = new Time(10, 14, 43);
+        System.out.println(time4.info());
+        Time time5 = new Time(8, 30, 29);
+        time4.add(time5.info());
+        System.out.println(time4.info());
+
+
     }
 }
--- src/Time.java
+++ src/Time.java
@@ -10,14 +10,53 @@ public class Time {
     }
 
     public void tick() {
+        second++;
+        if (second > 59) {
+            second = 0;
+            minute++;
+        }
+        if (minute > 59) {
+            minute = 0;
+            hour++;
+        }
+        if (hour > 23) {
+            hour = 0;
+        }
 
     }
 
-    public void add(Time time2) {
-
+    public void add(String time2) {
+        second = second + Integer.parseInt(time2.substring(6));
+        minute = minute + Integer.parseInt(time2.substring(3, 5));
+        hour = hour + Integer.parseInt(time2.substring(0, 2));
+        while (second > 59) {
+            second -= 60;
+            minute++;
+        } while (minute > 59) {
+            minute -= 60;
+            hour++;
+        } while (hour > 23) {
+            hour -= 24;
+        }
     }
 
     public String info() {
-        return "";
+        String info = "";
+        if (hour < 10) {
+            info+= "0" + hour + ":";
+        } else {
+            info+= hour + ":";
+        }
+        if (minute < 10) {
+            info+= "0" + minute + ":";
+        } else {
+            info+= minute + ":";
+        }
+        if (second < 10) {
+            info+= "0" + second;
+        } else {
+            info+= second;
+        }
+        return info;
     }
 }
